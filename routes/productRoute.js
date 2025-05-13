a89103d3c7cfb0a76ebbd3f756c08761409fe7d9
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-05-13 22:00:28 +0530

    add new functions to product 📍

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { deleteProduct, getProduct, saveProduct } from '../controller/productController.js';
+import { deleteProduct, getProduct, saveProduct, updateProduct,getProductById } from '../controller/productController.js';
 
 const productRouter = express.Router();
 
 productRouter.get("/", getProduct);
 productRouter.post("/", saveProduct);
 productRouter.delete("/:productId", deleteProduct)
+productRouter.put("/:productId" , updateProduct)
+productRouter.get("/:productId" , getProductById)
+
 
 export default productRouter;
