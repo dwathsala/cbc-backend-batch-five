635da480e5bae3b5b26b92d0d35816471611d492
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-05-23 14:03:14 +0530

    create review files📍

---
+++ models/review.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const reviewSchema = mongoose.Schema({
+    productId: {
+        type: String,
+        required: true
+    },
+    userEmail: {
+        type: String,
+        required: true
+    },
+    userName: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Review = mongoose.model("reviews", reviewSchema);
+export default Review;
