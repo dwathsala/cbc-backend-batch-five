803d9dedc1748ed3483ecd72fb6ee17bfa1e2a18
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-11-24 21:59:29 +0530

    import isAdmin into productController.js🔗

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,5 +1,6 @@
 import Order from "../models/order.js"
 import Product from "../models/product.js"
+import { isAdmin } from "./userController.js";
 
 export async function createOrder(req,res) {
       //get user information
